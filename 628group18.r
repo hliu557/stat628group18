b576575fb76ffd453720c7131b22646cbde5ad42
HongyiLiu <2509841336@qq.com>
2020-11-21 10:45:45 +0800

    second verssioon of R

--- 628group18.r
+++ 628group18.r
@@ -1,7 +1,16 @@
+rm(list=ls())
+library(rjson)
+library(jsonlite)
 library(tidytext)
 library(dplyr)
 library(stringr)
 library(plyr)
+
+user <- jsonlite::stream_in(file("user_city.json"))
+tip <- jsonlite::stream_in(file("tip_city.json"))
+review <- jsonlite::stream_in(file("review_city.json"))
+business <- jsonlite::stream_in(file("business_city.json"))
+
 Chinese_food <- grep(pattern = c('Chinese'),business$categories[business$is_open==1])
 #Chinese resteraunt still open
 Chinese_food_business_ID <-business$business_id[Chinese_food]
@@ -16,19 +25,22 @@ text<-Chinese_food_review$text
 text<-gsub(pattern = c('[0-9[:punct:]]+?'),replacement ="",text)
 #remove number
 id<-Chinese_food_review$review_id
-review_id <- data.frame(line = id, text = text)
+review_and_id <- data.frame(line = id, text = text)
 
-k<-unnest_tokens(review_id,word, text)
+k<-unnest_tokens(review_and_id,word, text)
 word <- count(k$word)$x
 p<-length(word)
 n<-length(text)
 
 reviw_to_word<-matrix(ncol = p,nrow = n)
 stat<-Sys.time()
-for (i in 1:length(text)){
-
+row_name<-rep(0,p)
+for (i in 1:n){
+  
   index <-which(word%in%k$word[(k$line==id[i])])
-  reviw_to_word[i,index] <- count(k$word[(k$line==id[i])])$freq
+  vector<-rep(0,p)
+  vector[index] <- count(k$word[(k$line==id[i])])$freq
+  row_name<-rbind(row_name,vector)
 }
 end<-Sys.time()
 end-stat
